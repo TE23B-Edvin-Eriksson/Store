9b6318dec09d9923b69f8e92c687b69e03bd4de6
Edvin Eriksson <edvin.eriksson@elev.ga.ntig.se>
2024-10-08 12:17:36 +0200

    Microtransaction Stoe första push

---
+++ Store/Program.cs
@@ -0,0 +1,41 @@
+﻿int i = 0;
+while (i < 10)
+{
+    Console.WriteLine("Hej!");
+    i ++;
+}
+
+
+for (int j=0; j < 10; j++)
+{
+    Console.WriteLine("Hej!");
+}
+
+
+
+
+int ageNum = 0;
+bool success = false;
+
+while (success != true)
+{
+    Console.WriteLine("Age:");
+    string age = Console.ReadLine();
+
+
+    success = int.TryParse(age, out ageNum);
+
+    if (success == false)
+    {
+        Console.WriteLine("Skriv en siffra!");
+    }
+}
+
+if (ageNum >= 18)
+{
+    Console.WriteLine("Welcome!");
+}
+
+
+
+Console.ReadLine();
